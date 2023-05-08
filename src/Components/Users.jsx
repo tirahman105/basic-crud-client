c9d59cbce76ed65542602f67ca045c1619033804
tirahman105 <tirahman105@gmail.com>
2023-05-08 12:57:31 +0600

    basic crud client

---
+++ src/Components/Users.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
+
+const Users = () => {
+    // const users = useLoaderData()
+
+    const loadedUsers = useLoaderData();
+    const [users, setUsers] = useState(loadedUsers);
+
+    const handleDelete = _id =>{
+        console.log( 'delete', _id);
+
+        fetch(`http://localhost:5000/users/${_id}`, {
+            method: 'DELETE'
+        })
+        .then(res=> res.json())
+        .then(data => {
+            console.log(data);
+            if(data.deletedCount>0){
+                alert('deleted successfully');
+                const remaining = users.filter(user => user._id !== _id);
+                setUsers(remaining); 
+            }
+        })
+    }
+    return (
+        <div>
+            <h1>Total users:{users.length} </h1>
+            <div>
+                {
+                    users.map(user => <p
+                    key={user._id}>
+                        {user.name} : {user.email} : id: {user._id} 
+                        
+                        <Link to={`/update/${user._id}`}>
+                            <button>Update</button>
+                        </Link>
+                        
+                         <button
+                            onClick={ () => handleDelete(user._id)}
+                        >X</button>
+                    </p>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Users;
